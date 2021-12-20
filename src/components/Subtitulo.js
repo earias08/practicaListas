b335d4148279014adaa91006958da2761c86b36f
Emilse <emilsearias@gmail.com>
2021-12-20 17:29:18 -0300

    se agrego el props en subtitulo

--- src/components/Subtitulo.js
+++ src/components/Subtitulo.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const Subtitulo = () => {
+const Subtitulo = (props) => {
+    //aqui va la logica
+
     return (
+        //aqui va el maquetado y un poquito de logica
         <div>
-            <h3 className='text-center'>Tareas de la comision</h3>
+            <h3 className='text-center'>Tareas de la comision {props.comision}</h3>
         </div>
     );
 };
